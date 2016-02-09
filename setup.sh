029b3d3fb4ec488b24d2dbf98a436080c97f6e8e
Gustavo Stor <gsa2@cin.ufpe.br>
2016-02-09 10:20:15 -0300

    wip

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+FORCE=false
+START=true
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+  case $key in
+    -f|--force)
+      FORCE=true
+      ;;
+    -n|--nostart)
+      START=false
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+
+function bootstrap {
+  npm install
+}
+
+FILENAME=$PWD/.last_boot
+LASTTIME=0
+
+echo $FILENAME
+if [ -f $FILENAME ]; then
+  LASTTIME=$(<$FILENAME)
+fi
+
+THRESHOLDMILLIS=$((3*24*60*60))
+TODAYMILLIS=$(date +%s)
+COMPTIMEMILLIS=$(($TODAYMILLIS-$THRESHOLDMILLIS))
+
+if $FORCE || (($LASTTIME < $COMPTIMEMILLIS));
+then
+  bootstrap
+fi
+
+echo $TODAYMILLIS > $FILENAME
+
+if $START; then
+  nodemon judger
+fi
+
