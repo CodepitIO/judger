ec325db7c8406a610f1f4a82bf7d5d206d91a538
Gustavo Stor <gustavostor@gmail.com>
2016-07-08 14:32:23 -0300

    updated contest model

--- src/models/contest.js
+++ src/models/contest.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose'),
   _ = require('lodash')
 
-const Submission = require('./submission')
+const Submission = require('./submission'),
+  ValidateChain = require('../utils/utils').validateChain
 
 const ObjectId = mongoose.Schema.Types.ObjectId
 
@@ -20,7 +21,6 @@ let contestantSchema = mongoose.Schema({
 
 let schema = mongoose.Schema({
   name: String,
-  description: String,
 
   author: {
     type: ObjectId,
@@ -60,41 +60,48 @@ let schema = mongoose.Schema({
   timestamps: true
 })
 
-schema.index({
-  date_start: -1
-})
-schema.index({
-  date_end: -1
-})
-schema.index({
-  author: 1,
-  createdAt: -1
-})
+schema.index({ date_start: -1 })
+schema.index({ date_end: -1 })
+schema.index({ author: 1, createdAt: -1 })
+schema.index({ 'contestants.id': 1, 'date_start': -1, })
+schema.index({ 'contestants.id': 1, 'date_end': -1, })
+schema.index({ 'contestants.id': 1, '_id': 1, })
+schema.index({ 'contestants.team': 1, 'contestants.id': 1, '_id': 1, })
 
-schema.methods.getUserRepresentative = function (id) {
-  if (!id) return false
-  var elem = _.find(this.contestants, function (obj) {
-    return obj.id && obj.id.toString() === id.toString()
+schema.methods.getUserRepresentative = function (user) {
+  let id = user && user._id && _.toString(user._id) || _.toString(user)
+  let elem = _.find(this.contestants, function (obj) {
+    return obj.id && _.toString(obj.id) === id
   })
   return elem && (elem.team || elem.id)
 }
 
-schema.methods.userInContest = function (id) {
-  if (!id) return false
-  id = id.toString()
+schema.methods.userInContest = function (user) {
+  let id = user && user._id && _.toString(user._id) || _.toString(user)
   return _.some(this.contestants, (obj) => {
-    let cid = obj.id && obj.id._id ? obj.id._id.toString() :
-              obj.id ? obj.id.toString() :
-              obj.toString()
-    return cid === id
+    let uid = (obj.id && obj.id._id) ? obj.id._id : (obj.id ? obj.id : obj)
+    return _.toString(uid) === id
   })
 }
 
-schema.methods.problemInContest = function (id) {
-  if (!id) return false
+schema.methods.problemInContest = function (problem) {
+  let id = problem && problem._id && _.toString(problem._id) || _.toString(problem)
   return _.some(this.problems, (obj) => {
-    return obj.toString() === id.toString()
+    return _.toString(obj) === _.toString(id)
   })
+}
+
+schema.methods.isAuthor = function(user) {
+  let id = user && user._id && _.toString(user._id) || _.toString(user)
+  return this.author.toString() === id
+}
+
+schema.methods.hasStarted = function() {
+  return this.date_start <= new Date()
+}
+
+schema.methods.hasEnded = function() {
+  return this.date_end <= new Date()
 }
 
 module.exports = mongoose.model('Contest', schema)
