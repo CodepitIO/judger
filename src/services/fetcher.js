412e50b1f08081983a2190f39d057eae167ebfd0
Gustavo Stor <gustavostor@gmail.com>
2016-08-25 15:22:03 -0300

    Handling error with null fetched problems object

--- src/services/fetcher.js
+++ src/services/fetcher.js
@@ -102,6 +102,7 @@ module.exports = (() => {
     return callback(null, (done) => {
       oj.fetchProblems((err, fetched) => {
         console.log(oj.type)
+        if (!fetched) err = err || new Error()
         if (err) console.log(err)
         else console.log(fetched.length + ' problems')
         return done(err, fetched)
